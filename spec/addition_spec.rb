494e48f5b8ddb83d41865ea2c19de5f8f61ccf2f
Rahul Srivastava <rahul.iiita94@gmail.com>
2015-06-08 18:00:28 +0530

    execute functionality added in Addition class

--- spec/addition_spec.rb
+++ spec/addition_spec.rb
@@ -5,4 +5,10 @@ describe Addition do
     additon = Addition.new(5)
     expect(additon.operand).to eq(5)
   end  
+
+  it 'should return 10 when initialised with 5 and executed with 5' do
+    additon = Addition.new(5)
+    expect(additon.execute(5)).to eq(10)
+  end
+
 end
