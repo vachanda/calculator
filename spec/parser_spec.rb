fbd9f370ce5c0eb69f5d0fa66b1f2c58c4ede311
Rahul Srivastava <rahul.iiita94@gmail.com>
2015-06-08 16:29:18 +0530

    removed dead code

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,18 +7,4 @@ describe Parser do
     command = 'add 5'
     expect(parser.read_command(command)).to eq(result)
   end 
-
-  it 'should parse subtract 5 as an array containing subtract and 5 as elements' do
-    parser = Parser.new
-    result = ['subtract', '5']
-    command = 'subtract 5'
-    expect(parser.read_command(command)).to eq(result)
-  end 
-
-   it 'should parse multiply 5 as an array containing mutliply and 5 as elements' do
-    parser = Parser.new
-    result = ['multiply', '5']
-    command = 'multiply 5'
-    expect(parser.read_command(command)).to eq(result)
-  end
 end
